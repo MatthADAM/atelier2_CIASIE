e8c283bc5e5448c803a88fe991b4939b00cd868a
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-03-22 12:02:42 +0100

    marche pas

--- app_vue/src/components/Homepage.vue
+++ app_vue/src/components/Homepage.vue
@@ -9,7 +9,7 @@
             ref="map"
             style="width:900px;height:400px;">
             <l-tile-layer :url="osmurl"></l-tile-layer>
-            <!-- <l-marker v-for="mark in markers" :key="mark.id" :lat-lng="mark.coordinates"></l-marker> -->
+            <l-marker :lat-lng="coord"></l-marker>
         </l-map>
         <br/>
         <router-link to="/connexion" tag="button" class="btn btn-outline-primary"> Se connecter </router-link>
@@ -21,17 +21,38 @@
 
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
+import axios from 'axios';
 
     export default {
+        created () {
+            /* this.markers.forEach(element => {
+                axios
+                .get("https://api-adresse.data.gouv.fr/search/?q=" + element.adress.replace(/ /g, "+") + "&postcode=" + element.postalCode)
+                .then(response => {
+                    this.coord[0] = response.data.features[0].geometry.coordinates[1];
+                    this.coord[1] = response.data.features[0].geometry.coordinates[0];
+                    console.log(this.coord);
+                });
+            }); */
+
+            axios
+                .get("https://api-adresse.data.gouv.fr/search/?q=" + this.test.adress.replace(/ /g, "+") + "&postcode=" + this.test.postalCode)
+                .then(response => {
+                    this.coord[0] = response.data.features[0].geometry.coordinates[1];
+                    this.coord[1] = response.data.features[0].geometry.coordinates[0];
+                    console.log(this.coord);
+                });
+        },
         data () {
             return {
                 center: [48.688418, 6.1825940],
                 zoom: 12,
                 osmurl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                 markers: [
-                    {id:1,coordinates: [48.693552, 6.183264]},
-                    {id:2,coordinates: [48.688080, 6.180562]},
+                    {id:1,name:"Test1", adress: "37 Rue du général frère", postalCode:"54500"},
                 ],
+                coord:[],
+                test : {id:1,name:"Test1", adress: "37 Rue du général frère", postalCode:"54500"},
             }
         },
         components: {LMap, LTileLayer, LMarker},   
