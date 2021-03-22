4325682b54b98c3f58b72d7ce2bfc5b203c246bd
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-03-22 15:50:49 +0100

    liste affichée

--- app_vue/src/components/Homepage.vue
+++ app_vue/src/components/Homepage.vue
@@ -7,9 +7,11 @@
             :zoom="zoom"
             class="map"
             ref="map"
-            style="width:900px;height:400px;">
+            style="width:900px;height:400px;"
+            v-if="coord.length == markers.length">
             <l-tile-layer :url="osmurl"></l-tile-layer>
-            <l-marker :lat-lng="coord" v-if="coord.length > 0"></l-marker>
+            <l-marker v-for="(item,index) in markers" :key="item.name" :lat-lng="coord[index]">
+            </l-marker>
         </l-map>
         <br/>
         <p>{{coord}}</p>
@@ -20,7 +22,7 @@
 
 <script>
 
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LIcon} from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 
@@ -28,15 +30,17 @@ import axios from 'axios';
         methods: {
         },
         created () {
-            axios
-                .get("https://api-adresse.data.gouv.fr/search/?q=" + this.test.adress.replace(/ /g, "+") + "&postcode=" + this.test.postalCode)
+            this.markers.forEach(element => {
+                axios
+                .get("https://api-adresse.data.gouv.fr/search/?q=" + element.adress.replace(/ /g, "+") + "&postcode=" + element.postalCode)
                 .then(response => {
                     this.lat = response.data.features[0].geometry.coordinates[1];
                     this.lon = response.data.features[0].geometry.coordinates[0];
-                    this.coord = [this.lat,this.lon];
+                    this.coord.push([this.lat,this.lon]);
                     console.log(this.coord);
-                    console.log(this.gne);
-                });
+                });    
+            });
+            
         },
         data () {
             return {
@@ -45,15 +49,14 @@ import axios from 'axios';
                 osmurl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                 markers: [
                     {id:1,name:"Test1", adress: "37 Rue du général frère", postalCode:"54500"},
+                    {id:2,name:"Test2", adress: "2Ter Boulevard Charlemagne", postalCode:"54000"},
                 ],
                 lat:0,
                 lon:0,
                 coord:[],
-                test : {id:1,name:"Test1", adress: "37 Rue du général frère", postalCode:"54500"},
-                gne: [48.660233, 6.170445],
             }
         },
-        components: {LMap, LTileLayer, LMarker},   
+        components: {LMap, LTileLayer, LMarker, LIcon},   
     }
 </script>
 
