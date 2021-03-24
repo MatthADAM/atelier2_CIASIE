10d75e4c076abf63c2b590147728d0d7d94f4114
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-03-24 16:23:58 +0100

    sort event list

--- app_vue/src/components/Eventlist.vue
+++ app_vue/src/components/Eventlist.vue
@@ -5,14 +5,16 @@
         <table class="table table-striped">
             <thead>
             <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Adress</th>
-                <th scope="col">PostCode</th>
+                <th v-bind:class="order === 1 ? 'descending' : 'ascending'" class="clickable" v-on:click="order = order * - 1" scope="col">Créateur</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Adresse</th>
+                <th scope="col">Code Postal</th>
                 <th scope="col">Date</th>
             </tr>
             </thead>
             <tbody>
-            <tr scope="row" v-for="item in res" :key="item.name">
+            <tr scope="row" v-for="item in filterOwner" :key="item.name">
+                <td>{{item.owner}}</td>
                 <td>{{item.name}}</td>
                 <td>{{item.adress}}</td>
                 <td>{{item.postCode}}</td>
@@ -27,9 +29,31 @@
 import Navbar from './Navbar.vue'
 import $ from 'jquery'
     export default {
+        methods: {
+        },
+        computed: {
+            filterOwner() {
+                var part1;
+                var part2;
+                if (this.order == 1) {
+                    part1 = this.res.filter(log => log.owner == this.$session.get("log"));
+                    part2 = this.res.filter(log => log.owner != this.$session.get("log"));
+                } else {
+                    part1 = this.res.filter(log => log.owner != this.$session.get("log"));
+                    part2 = this.res.filter(log => log.owner == this.$session.get("log"));
+                }
+                this.res = part1;
+                part2.forEach(element => {
+                    this.res.push(element);
+                });
+                console.log(this.res);
+                return this.res;
+            }
+        },
         data () {
             return {
                 res: [],
+                order: -1,
             }
         },
         created () {
@@ -57,5 +81,15 @@ import $ from 'jquery'
 </script>
 
 <style >
-
+.clickable:hover {
+    cursor:pointer;
+}
+.ascending:after {
+    content: "\25B2";
+    padding-left: 15px;
+}
+.descending:after {
+    content: "\25BC";
+    padding-left: 15px;
+}
 </style>
