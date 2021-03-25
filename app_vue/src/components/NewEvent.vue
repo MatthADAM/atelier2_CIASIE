b74433dbbaa62f2e34c5f7fb60394cf252a29540
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-03-25 09:58:40 +0100

    creer event ok

--- app_vue/src/components/NewEvent.vue
+++ app_vue/src/components/NewEvent.vue
@@ -6,16 +6,26 @@
             <div class="form-connect shadow p-3 mb-5 bg-white rounded">
                 <form>
                     <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="email" required>
-                        <small id="emailHelp" class="form-text text-muted">This email is personnal don't share it</small>
+                        <label for="name">Name of event</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter name" v-model="name" required>
                     </div>
                     <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Password" v-model="pwd" required>
+                        <label for="adress">Adress</label>
+                        <input type="text" class="form-control" id="adress" placeholder="Adress of event" v-model="adress" required>
                     </div>
-                    <button type="submit" class="btn btn-primary" v-on:click="connexion">Connexion</button>
-                    <router-link to="/inscription">Inscription</router-link>
+                    <div class="form-group">
+                        <label for="postCode">PostCode</label>
+                        <input type="number" class="form-control" id="postCode" placeholder="PostCode of event" v-model="postCode" required>
+                    </div>
+                    <div>
+                        <label for="public">Event public</label>
+                        <input type="number" min="0" max="1" class="form-control" id="public" v-model="publicEvent" required>
+                    </div>
+                    <div>
+                        <label for="date">Date of event</label>
+                        <input type="date" class="form-control" id="date" v-model="date" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary" v-on:click="createEvent">Créer</button>
                 </form>
             </div>
         </div>
@@ -24,13 +34,40 @@
 
 <script>
 import Navbar from './Navbar.vue'
+import axios from 'axios'
     export default {
+        data () {
+            return {
+                name:"",
+                adress:"",
+                postCode:null,
+                publicEvent:false,
+                date:null,
+            }
+        },
         beforeCreate: function () {
             if (!this.$session.exists()) {
             this.$router.push('/')
             }
         },
         components: {Navbar},
+        methods: {
+            createEvent() {
+                axios.post("http://docketu.iutnc.univ-lorraine.fr:11501/api/addEvent", {
+                    owner:this.$session.get("log"),
+                    name :this.name,
+                    adress:this.adress,
+                    postCode:this.postCode,
+                    public: this.publicEvent,
+                    date: this.date,
+                    token: "fzefzefzefezfzefez"
+                })
+                .then(function (response) {
+                    console.log(response);
+                    alert("Event " + this.name + " créé !");
+                });
+            }
+        },
     }
 </script>
 
