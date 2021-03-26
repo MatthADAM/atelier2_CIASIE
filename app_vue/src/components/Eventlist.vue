185990f65b64f7d05e3068ed7f691e5ce7f6deb7
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-03-26 15:22:44 +0100

    debut accept invit

--- app_vue/src/components/Eventlist.vue
+++ app_vue/src/components/Eventlist.vue
@@ -5,20 +5,23 @@
         <table class="table table-striped">
             <thead>
             <tr>
-                <th v-bind:class="order === 1 ? 'descending' : 'ascending'" class="clickable" v-on:click="order = order * - 1" scope="col">Créateur</th>
+                <th scope="col">Créateur</th>
                 <th scope="col">Nom</th>
                 <th scope="col">Adresse</th>
                 <th scope="col">Code Postal</th>
                 <th scope="col">Date/Heure</th>
+                <th scope="col">Status</th>
             </tr>
             </thead>
             <tbody>
-            <tr scope="row" v-for="item in filterOwner" :key="item.name">
+            <tr scope="row" v-for="(item,index) in filterOwner" :key="index">
                 <td>{{item.owner}}</td>
                 <td>{{item.name}}</td>
                 <td>{{item.adress}}</td>
                 <td>{{item.postCode}}</td>
                 <td>{{item.date}}</td>
+                <!-- <td v-if="invitAcceptee[index] == false"><button type="button" class="btn btn-success">Accepter</button></td>
+                <td v-else><button type="button" class="btn btn-outline-success">Acceptée</button></td> -->
             </tr>
             </tbody>
         </table>
@@ -41,19 +44,6 @@ import $ from 'jquery'
         },
         computed: {
             filterOwner() {
-                var part1;
-                var part2;
-                if (this.order == 1) {
-                    part1 = this.res.filter(log => log.owner == this.$session.get("log"));
-                    part2 = this.res.filter(log => log.owner != this.$session.get("log"));
-                } else {
-                    part1 = this.res.filter(log => log.owner != this.$session.get("log"));
-                    part2 = this.res.filter(log => log.owner == this.$session.get("log"));
-                }
-                this.res = part1;
-                part2.forEach(element => {
-                    this.res.push(element);
-                });
                 return this.res.filter((row, index) => {
 		            let start = (this.currentPage-1)*this.pageSize;
 		            let end = this.currentPage*this.pageSize;
@@ -81,15 +71,6 @@ import $ from 'jquery'
                 url: "http://docketu.iutnc.univ-lorraine.fr:11501/api/event?public=1&owner=" + this.$session.get("log"),
                 success: function (result) {
                     tab = result;
-                },
-                async: false
-            });
-            $.ajax({
-                url: "http://docketu.iutnc.univ-lorraine.fr:11501/api/event/owner/" + this.$session.get("log"),
-                success: function (result) {
-                    result.forEach(element => {
-                        tab.push(element);
-                    });
                 },
                 async: false
             });
@@ -128,13 +109,5 @@ import $ from 'jquery'
 <style >
 .clickable:hover {
     cursor:pointer;
-}
-.ascending:after {
-    content: "\25B2";
-    padding-left: 15px;
-}
-.descending:after {
-    content: "\25BC";
-    padding-left: 15px;
 }
 </style>
